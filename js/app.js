9c705365c185ca69203ccdfe187a4ea6e066707c
Yuale <yualelm@gmail.com>
2023-01-04 15:39:05 -0400

    Terminado

--- js/app.js
+++ js/app.js
@@ -1,19 +1,64 @@
 "use strict";
 // Variables
-const resultado = document.querySelector("#resultado");
+const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+const datosBusqueda = {
+    marca: "",
+    year: "",
+    minimo: "",
+    maximo: "",
+    puertas: "",
+    transmision: "",
+    color: ""
+};
+const resultado = document.querySelector("#resultado");
+const max = new Date().getFullYear();
+const min = max - 15;
 
-
-// Eventos
+// Espera a la carga de la pagina para manipular el DOM
 document.addEventListener("DOMContentLoaded", () => {
-    mostrarAutos(); // Muestra los automoviles al cargar
+    mostrarAutos(autos); // Muestra los automoviles al cargar
     llenarSelect(); // Llena las opciones de años
-
 });
 
+// Eventos
+marca.addEventListener("change", (event) => {
+    datosBusqueda.marca = event.target.value;
+    filtrarAuto();
+}); 
+year.addEventListener("change", (event) => {
+    datosBusqueda.year = event.target.value;
+    filtrarAuto();
+}); 
+minimo.addEventListener("change", (event) => {
+    datosBusqueda.minimo = event.target.value;
+    filtrarAuto();
+}); 
+maximo.addEventListener("change", (event) => {
+    datosBusqueda.maximo = event.target.value;
+    filtrarAuto();
+}); 
+puertas.addEventListener("change", (event) => {
+    datosBusqueda.puertas = event.target.value;
+    filtrarAuto();
+}); 
+transmision.addEventListener("change", (event) => {
+    datosBusqueda.transmision = event.target.value;
+    filtrarAuto();
+}); 
+color.addEventListener("change", (event) => {
+    datosBusqueda.color = event.target.value;
+    filtrarAuto();
+}); 
 
 // Funciones
-function mostrarAutos() {
+function mostrarAutos(autos) {
+    limpiarHTML();
     autos.forEach( auto => {
         const { marca, modelo, year, puertas, precio, color, transmision } = auto;
         const autoHTML = document.createElement("P");
@@ -24,6 +69,83 @@ function mostrarAutos() {
     });
 };
 
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+};
+
 function llenarSelect() {
-    console.log("Llenando el select");
+    for(let i = max; i > min; i--) {
+        const opcion = document.createElement("option");
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega cada año al select
+    };
+};
+
+function filtrarAuto() {
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter( fitrarMinimo ).filter( filtrarMaximo ).filter( filtrarPuertas ).filter( filtrarTransmision ).filter( filtrarColor );
+    if(resultado.length) {
+        mostrarAutos(resultado);
+    } else {
+        noResultado();
+    };
+};
+
+function filtrarMarca(auto) {
+    if(datosBusqueda.marca) {
+        return auto.marca === datosBusqueda.marca;
+    };
+    return auto;
+};
+
+function filtrarYear(auto) {
+    if(datosBusqueda.year) {
+        return auto.year === parseInt(datosBusqueda.year);
+    };
+    return auto;
+};
+
+function fitrarMinimo(auto) {
+    if(datosBusqueda.minimo) {
+        return auto.precio >= parseInt(datosBusqueda.minimo);
+    };
+    return auto;
+};
+
+function filtrarMaximo(auto) {
+    if(datosBusqueda.maximo) {
+        return auto.precio <= parseInt(datosBusqueda.maximo);
+    };
+    return auto;
+};
+
+function filtrarPuertas(auto) {
+    if(datosBusqueda.puertas) {
+        return auto.puertas === parseInt(datosBusqueda.puertas);
+    };
+    return auto;
+};
+
+function filtrarTransmision(auto) {
+    if(datosBusqueda.transmision) {
+        return auto.transmision === datosBusqueda.transmision;
+    };
+    return auto;
+};
+
+function filtrarColor(auto) {
+    if(datosBusqueda.color) {
+        return auto.color === datosBusqueda.color;
+    };
+    return auto;
+};
+
+function noResultado() {
+    limpiarHTML();
+    const noResultado = document.createElement("DIV");
+    noResultado.classList.add("alerta","error");
+    noResultado.textContent = "No hay resultados";
+    resultado.appendChild(noResultado);
 };
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
 # Buscador de autos - JavaScritp
-``  
 Este es un simple buscador de autos que funciona unicamente por filtros, el html y el css son una plantilla web, el archipo importante es el `app.js`, tambien hay un `db.js`  pero ese archivo solo almacena los objetos de la "base de datos", es decir, los vehiculos.
 
 La fucion que muestra los autos de la base de datos esperara a que todo el contenido del DOM carge antes de mostrar los vehiculos.
@@ -8,8 +7,26 @@ La fucion que muestra los autos de la base de datos esperara a que todo el conte
 1. **const resultado**: Selecciona una etiqueta h1 con el id resultado en la linea 101, dentro de esta etiqueta es donde se mostratan los resultados de la busqueda.
 2. **const { }**: Destructuring sobre todos los metadatos del objeto auto.
 3. **const autoHTML**: crea una etiqueta `p` sobre cada objeto dentro de `db.js` y los introduce en `const resultado` de manera ordenada.
-4. **const year**: 
-
+4. **const year**: Selecciona una etiqueta `select` con el id *year*.
+5. **const max**: Esta variable tiene un metodo que toma el año actual.
+6. **const min**: Esta variable toma la variable `max` y le resta 15.
+7. **const opcion**: Al ejecutarse la funcion `llenarselect()`. Esta variable toma el valor del año en cada iteracion para agregarlo al HTML.
+8. **const datosBusqueda**: Este es un objeto que agrupa todos los parametros de busqueda.
+9. **const marca**:  Selecciona una etiqueta `select` con el id *marca*.
+10. **const minimo**:  Selecciona una etiqueta `select` con el id *minimo*.
+11. **const maximo**:  Selecciona una etiqueta `select` con el id *maximo*.
+12. **const puertas**:  Selecciona una etiqueta `select` con el id *puertas*.
+13. **const transmision**:  Selecciona una etiqueta `select` con el id *transmision*.
+14. **const color**:  Selecciona una etiqueta `select` con el id *color*.
 
 ## Funciones y ejecucion 
 1. **mostrarAutos()**: Esta funcion toma los datos de la "base de datos" y los muestra mediante la variable `const resultado`. La funcion contiene un iterador `.forEach` que revisa cada elemento, luego crea una etiqueta `P` con los datos del vehiculo y los introduce en la variable seleccionada.
+2. **llenarSelet()**: Esta funcion itera sobre dos variables `min` y `max` para crear un rango de años y despues llevarlo al campo de seleccion del html.
+3. **filtrarAuto()**: Esta funcion encadena todos los filtros en una unica variable para que se ejecuten segun lo que se seleccione.
+4. **filtrarMarca()**: Filtra los autos por Marca.
+5. **filtrarYear()**: Filtra los autos por año.
+6. **filtrarMaximo()**: Filtra los autos por precio Maximo.
+7. **filtrarMinimo()**: Filtra los autos por precio Minimo.
+8. **filtrarPuertas()**: Filtra los autos por numero de puertas.
+9. **filtrarTransmision()**: Filtra los autos por tipo de Transmision.
+10. **filtrarColor()**: Filtra los autos por color.
